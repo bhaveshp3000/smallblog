7fbd763a30f8b2deffc757e0fda4f73f6cc003fe
Bhavesh Patel <bhaveshp3000@gmail.com>
2018-06-12 11:58:35 +0530

    Databse for user added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,flash,url_for
 from app.forms import LoginForm
 
 
@@ -16,5 +16,8 @@ def index():
 @app.route('/login',methods=['post','get'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
+        return redirect(url_for('/index'))
 
     return render_template('login.html', title='Sign In', form=form)
